68555d51d8315103db8907d8558bc1671eb1e1f7
Mars <mallorysmith6464@gmail.com>
2019-09-17 10:29:23 -0400

    started solving kata

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace mumbling_kata
+{
+  public class Accumul
+  {
+    public static String Accum(string s)
+    {
+      //made variable
+      var result = s.Split("").Select((letter, index) =>
+      {
+        //return variable as uppercase then lowercase letters and then repeat that
+        return letter.ToUpperCase() + letter.ToLowerCase()
+        .Enumerable.repeat(index);
+        //add hyphen between letters when repeating letters is done
+      }); 'string.Join(string, params s[])';
+      //   string.Join("-");
+
+      Console.WriteLine(result);
+      return result;
+    }
+  }
+}
+static void Main(string[] args)
+{
+  Console.WriteLine("Hello World!");
+}
